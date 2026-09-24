2d492c6bcdab142011731fa0559e7eb78d159b50
Bhawesh Jain <bhaweshjain384@gmail.com>
2026-09-24 14:58:08 +0530

    Product Image Fix

--- src/lib/repositories/productRepository.ts
+++ src/lib/repositories/productRepository.ts
@@ -150,7 +150,8 @@ export class ProductRepository extends RepositoryBase {
           ON fallback_fl.id = (
                 SELECT id
                 FROM file_log
-                WHERE product_id = p.product_id
+                WHERE associated_id = p.product_id
+                  AND associated_type = 'product_image'
                   AND status = 1
                   AND company_id = ?
                 ORDER BY id ASC
